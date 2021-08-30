dfb215369d669119b58499d1adf3651884d3b7f2
Franco Mangone <francomangone18@gmail.com>
2021-08-30 00:30:35 -0300

    Index to email in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_023956) do
+ActiveRecord::Schema.define(version: 2021_08_30_032934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_08_30_023956) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "recipe_ingredients", "ingredients"
