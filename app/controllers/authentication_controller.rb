fb4628073786727697396ebcfe81336e4daf7cd1
Franco Mangone <francomangone18@gmail.com>
2021-09-04 17:31:46 -0300

    Added Set-Cookie header on /login response

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,19 @@ class AuthenticationController < ApplicationController
     command = AuthenticateUser.call(params[:email], params[:password])
  
     if command.success?
-      render json: { auth_token: command.result }
+      jwt = command.result
+
+      # Set the cookie on client by using a Set-Cookie header
+      response.set_cookie(
+        :recipes_app_jwt, # TODO: Change to [app_name]_jwt in the future
+        {
+          value: jwt,
+          expires: 30.minutes.from_now,
+          secure: Rails.env.production?,
+          httponly: Rails.env.production?
+        }
+      )
+      render json: { jwt: command.result }
     else
       render json: { error: command.errors }, status: :unauthorized
     end
