d156451f9a8e80d0a5380b2109993356f5c440fe
Franco Mangone <francomangone18@gmail.com>
2021-09-05 11:35:10 -0300

    Small refactor to AuthorizeApiRequest

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -12,8 +12,19 @@ class AuthorizeApiRequest
   private
 
   def user
-    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-    @user || errors.add(:token, 'Invalid token') && nil
+    if @decoded_auth_token.nil?
+      errors.add(:token, 'Invalid or missing token')
+      return nil
+    end
+
+    @user ||= User.find(decoded_auth_token[:user_id])
+
+    if @user.nil?
+      errors.add(:credentials, 'Invalid email or password')
+      return nil
+    end
+    
+    @user
   end
 
   def decoded_auth_token
